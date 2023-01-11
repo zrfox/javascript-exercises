c7301c033c1a451b563d16534781fae7f64c4b8a
Zachary Fox <zrfox97@gmail.com>
2023-01-11 14:56:37 -0800

    Complete Exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    
+    if(num<0)
+    {
+        return "ERROR"; 
+    }
+    let repeatedString ="";
+    
+    for(i = 0; i<num; i++)
+    {
+        repeatedString += string;
+    }
+    return repeatedString;
 };
 
 // Do not edit below this line
