dc9dd05b98cbd6863a106830e6ee3133095cef76
Zachary Fox <zrfox97@gmail.com>
2023-01-11 15:07:56 -0800

    Complete Exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
 
+    let newString ="";
+    for(i = string.length - 1;i>-1;i--)
+    {
+        newString +=string[i];
+    }
+    return newString;
 };
 
 // Do not edit below this line
