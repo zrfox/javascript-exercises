eeede3791f6d5a17bf2b5554ba1199d152382fd5
Zachary Fox <zrfox97@gmail.com>
2023-01-12 13:00:49 -0800

    Complete Exercise 5

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,19 @@
-const leapYears = function() {
+const leapYears = function(year) {
+
+    yearVar = year;
+
+    if(yearVar%4==0)
+    {
+        if(yearVar%400==0)
+            return true;
+        if(yearVar%100!=0)
+            return true;
+        else 
+        return false;
+    }
+    else{
+        return false;
+    }
 
 };
 
