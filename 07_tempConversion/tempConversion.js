9f564580b94d98fe9dc1a165596106356e8b25c4
Zachary Fox <zrfox97@gmail.com>
2023-01-12 15:04:19 -0800

    Complete Exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,24 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farenheit) {
+  let celsiusVar = 0;
+  celsiusVar = +((farenheit-32)*0.5556).toFixed(1)
+  if(celsiusVar==0.0)
+  {
+    celsiusVar = Math.trunc(celsiusVar);
+  }
+
+  return celsiusVar;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let farenheitVar = 0;
+          /*toFixed converts to string, + returns to number*/
+  farenheitVar = +(celsius*1.8+32).toFixed(1);
+  if(Number.isInteger(farenheitVar))
+  {
+    farenheitVar = Math.trunc(farenheitVar);
+  }
+  return farenheitVar;
+
 };
 
 // Do not edit below this line
