dec487e973c25f374becf5ddb9417dc25aa57018
Zachary Fox <zrfox97@gmail.com>
2023-05-30 19:11:14 -0700

    Finish Exercise 10. Fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    if(index == 1||index==2)
+    return 1;
+    else if(index<1)
+    return "OOPS";
+    let var1 = 1;
+    let var2 = 1;
+    for(let i = 0; i<index-2; i++)
+    {
+        if(i % 2 ==0)
+        var1 += var2;
+        else
+        var2 += var1;
+    }
+    if (var1 >var2)
+    return var1;
+    else
+    return var2;
 };
 
 // Do not edit below this line
